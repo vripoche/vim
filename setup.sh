7102eb7c2e5b1f1e748aeac4ee6c44a41ee1aa39
Vivien RIPOCHE <vivienripoche@gmail.com>
2023-07-12 11:29:55 +0200

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/vivienripoche/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 brew install git wget nvm neovim oh-my-posh
 brew install --cask google-chrome iterm2
-nvm install --lts
-nvm use --lts
 
 ssh-keygen -t ed25519 -C "vivienripoche@gmail.com"
 
-cp ./sc /usr/local/bin/
+sudo mkdir -P /usr/local/bin/
+sudo cp ./sc /usr/local/bin/
 
 echo '
 if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
@@ -20,6 +22,11 @@ alias vim="nvim"
 alias vi="nvim"
 
 export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 ' >> ~/.zshrc
+
+source ~/.zshrc
+
+nvm install --lts
+nvm use --lts
